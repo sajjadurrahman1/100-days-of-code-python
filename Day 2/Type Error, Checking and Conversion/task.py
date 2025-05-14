d5fc30aaaea3aefc7ac27112c0bbcbd47881b302
sajjad <sajjadurrahman130784@gmail.com>
2025-05-14 18:05:15 +0200

    type conversion

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,41 @@
-len(12345)
+#len("hello")
+#print(type("hello"))
+#int
+print(type(123))
+#float
+print(type(2.23))
+#str
+print(type("Bangladesh"))
+#bool
+print(type(True))
+#type conversion
+print(int("123") + int("234"))
+#in type conversion we can not convert a string and
+
+
+
+name_of_the_user=input("enter your name\n")
+print(type(name_of_the_user))#str
+length=len(name_of_the_user)#int
+print("number of letters in your name: " + str(length) )
+
+#type conversion
+x=1 #int
+y=2.2 #float
+z=1j #complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+a=float(x)
+print(type(a))
+
+b=int(y)
+print(type(b))
+
+c=complex(x)
+print(type(c))
+print(a)
+print(b)
+print(c)
