a310c543a2437b6c926f739e37e9a1edfda342c2
sajjad <sajjadurrahman130784@gmail.com>
2025-09-18 09:18:39 +0200

    functions with output

--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -1,7 +1,6 @@
-def format_string(f_name, l_name):
-    formated_f_name= f_name. title()
-    formated_l_name= l_name. title()
-    return f"{formated_f_name} {formated_l_name}"
-
-formated_name=(format_string("sajJAdur", "raHmAn"))
-print(formated_name)
+def function_1(text):
+    return text + text
+def function_2(text):
+    return text.title()
+output= function_2(function_1("gelo"))
+print(output)
